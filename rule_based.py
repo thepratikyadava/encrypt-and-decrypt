9c49fed00c56d112c96bb0d1c60662cbf92f8458
Pratik <thepratikyadava@gmail.com>
2026-06-23 12:17:02 +0530

    rule base encryption

--- rule_based.py
+++ rule_based.py
@@ -1,28 +1,33 @@
 import random
 import string
 char = " " + string.digits + string.punctuation + string.ascii_letters
-char = list(char)
+char  = list(char)
 key = char.copy()
 random.seed(42)
 random.shuffle(key)
-
-try:
+k = len(char)
+def encrypt():
+  try:
     user_input = input("Tell the message you want to encrpt:-")
     output = ""
     for letter in user_input :
         index = char.index(letter)
         output += key[index]
     print(output)    
-except Exception as err:
+  except Exception as err:
      print(f"An error found as {err}")
 
-try:
+def decrypt():
     user_input = input("Tell the message you want to decrypt:-")
     output = ""
     for letter in user_input :
         index = key.index(letter)
         output += char[index]
     print(output)
-except Exception as err:
- print(f"An error occured as {err}")
 
+
+a = int(input("Tell the operation you want to perform for encryption enter 1 and for decryption enter 2:-"))
+if a == 1 :
+   encrypt()
+else:
+   decrypt()
